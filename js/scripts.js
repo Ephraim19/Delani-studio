a35a2b2be20a6491a1919403520b4c2c27b45066
Ephraim19 <machayoephraim@gmail.com>
2021-12-17 10:26:46 +0300

    made the form textarea a little larger

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
   $("#designs").click(function () {
     $("#design").toggle();
-    $("#design-img").toggle()
+    $("#design-img").toggle();
   });
 });
 
 $(document).ready(function () {
-    $("#dev").click(function () {
-      $("#development").toggle();
-      $("#development-img").toggle()
-    });
+  $("#dev").click(function () {
+    $("#development").toggle();
+    $("#development-img").toggle();
   });
+});
 
-  $(document).ready(function () {
-    $("#products").click(function () {
-      $("#product").toggle();
-      $("#product-img").toggle()
-    });
+$(document).ready(function () {
+  $("#products").click(function () {
+    $("#product").toggle();
+    $("#product-img").toggle();
   });
+});
+
+$(document).ready(function () {
+  $('#work4').hover(function(){
+    $('#work4pop').popup()
+  })
+});
