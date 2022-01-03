7d8c0d67d1c447f37b78aee26c2ae21bab9770a8
Ephraim19 <machayoephraim@gmail.com>
2022-01-03 09:35:56 +0300

    Finished adding hover effect to portifolio section

--- js/scripts.js
+++ js/scripts.js
@@ -31,7 +31,73 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  $("#work4").hover(function () {
+  $("#work4").mouseover(function () {
     $("#work4pop").show();
+  });
+  $("#work4").mouseout(function () {
+    $("#work4pop").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#work3").mouseover(function () {
+    $("#work3pop").show();
+  });
+  $("#work3").mouseout(function () {
+    $("#work3pop").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#work2").mouseover(function () {
+    $("#work2pop").show();
+  });
+  $("#work2").mouseout(function () {
+    $("#work2pop").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#work1").mouseover(function () {
+    $("#work1pop").show();
+  });
+  $("#work1").mouseout(function () {
+    $("#work1pop").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#work5").mouseover(function () {
+    $("#work5pop").show();
+  });
+  $("#work5").mouseout(function () {
+    $("#work5pop").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#work6").mouseover(function () {
+    $("#work6pop").show();
+  });
+  $("#work6").mouseout(function () {
+    $("#work6pop").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#work7").mouseover(function () {
+    $("#work7pop").show();
+  });
+  $("#work7").mouseout(function () {
+    $("#work7pop").hide();
+  });
+});
+
+$(document).ready(function () {
+  $("#work8").mouseover(function () {
+    $("#work8pop").show();
+  });
+  $("#work8").mouseout(function () {
+    $("#work8pop").hide();
   });
 });
